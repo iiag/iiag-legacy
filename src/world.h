7104d3d0855ce62c5f87dc5e68ade10cf3ee1d16
Macri-man <admfav2012@gmail.com>
2014-01-18 17:35:34 -0500

    comments to things

--- src/world.h
+++ src/world.h
@@ -8,17 +8,17 @@
 #include "zone.h"
 #include "object.h"
 
-typedef struct {
-	object plyr;
-	zone ** zones;
-	form ** forms;
-	int form_count;
+typedef struct {//world states
+	object plyr;//player object
+	zone ** zones;//list of zones that exist
+	form ** forms;//list of monster and creatures and generic items that exit
+	int form_count;//number of forms
 } world_st;
 
-extern int width, height;
-extern world_st world;
+extern int width, height;//protoype variables
+extern world_st world;//prototype world
 
-void init_world(void);
+void init_world(void);//initializes the world
 
 #define PLYR world.plyr
 
