7bd363396a0b4cbfa62c21e3c0edefec5da050fa
Eric Richter <richteer@clarkson.edu>
2014-09-10 17:29:34 -0400

    minor changes

--- src/spells.c
+++ src/spells.c
@@ -1,6 +1,24 @@
 
 #include "spells.h"
 
+spell * spell_new()
+{
+	spell * sp = malloc(sizeof(spell));
+
+	char * name;
+	int level;
+
+	sp->caliber = 0;
+
+}
+
+spell * spell_upgrade(spell * one, spell * two)
+{
+
+
+}
+
+
 void spell_free(spell * sp)
 {
 	
--- src/spells.h
+++ src/spells.h
@@ -3,12 +3,28 @@
 
 typedef struct spell spell;
 
+// TODO: Define spell structure
 struct spell {
 	int caliber;
-	
+
+	char * 	
 
 };
 
-void spell_free(spell *);
+//
+// Allocate a new empty spell
+//
+spell * spell_new(void);
+
+//
+// Return a new spell from a combination of two, NULL if incompatible
+//
+spell * spell_upgrade(spell *, spell *);
+
+void spell_free(spell *)
+{
+
+	free(spell);
+}
 
 #endif
