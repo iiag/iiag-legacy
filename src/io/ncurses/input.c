34aeb978fe96ab6a5c1854620383d35f96981fb7
Chris Harding <hardinc@clarkson.edu>
2014-09-10 19:09:05 -0500

    Fixed direction prompt

--- src/io/ncurses/input.c
+++ src/io/ncurses/input.c
@@ -53,7 +53,7 @@ int nc_prompt_dir(const char * prompt, int * dx, int * dy)
 	wprintw(dispscr, "%s\n", prompt);
 	wrefresh(dispscr);
 
-	switch (input_get_ctrl()) {
+	switch (ctrl_by_key(input_get_ctrl())) {
 	case CTRL_LEFT:   *dx = -1; *dy = 0; break;
 	case CTRL_DOWN:   *dx =  0; *dy = 1; break;
 	case CTRL_UP:     *dx =  0; *dy =-1; break;
